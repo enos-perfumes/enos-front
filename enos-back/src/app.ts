3df14eaae170762b6e21a00b641bb9ed51ddc99a
Emmanuel Aaron Popa Campusano <100238224+emmacamp@users.noreply.github.com>
2024-06-11 19:16:40 -0400

    Delete enos-back directory

--- enos-back/src/app.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import cors from "cors";
-import perfumeRoutes from "./routes/perfume.routes";
-
-export const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use("/api/perfumes", perfumeRoutes);
